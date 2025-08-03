91fcc4b5d04ffbb86900cef88ca77c6669c25a12
MinaVaganteeee <98477967+MinaVaganteeee@users.noreply.github.com>
2025-08-03 21:28:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def check_new_videos():
 
         if channel_id not in latest_video_ids or latest_video_ids[channel_id] != video_id:
             latest_video_ids[channel_id] = video_id
-            send_telegram_message(f"📢 Nuovo video da {artist}:
+send_telegram_message(f"📢 Nuovo video da {artist}: {latest_title}\n{latest_link}")
 🎬 {video_title}
 🔗 {video_url}")
 
